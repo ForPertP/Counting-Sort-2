bee764832155b16c13d5e76fbb01443dc2678337
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-08 04:52:32 +0900

    Update Counting-Sort-2.cpp

--- Counting-Sort-2.cpp
+++ Counting-Sort-2.cpp
@@ -1,3 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
 template <typename T>
 void count_sort(T& arr)
 {
