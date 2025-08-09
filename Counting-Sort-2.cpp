fe9757e4f26ea66fe5f6b88ce2083983b20be67a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-10 08:55:13 +0900

    Update Counting-Sort-2.cpp

--- Counting-Sort-2.cpp
+++ Counting-Sort-2.cpp
@@ -13,6 +13,7 @@ vector<string> split(const string &);
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
+
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
@@ -68,4 +69,28 @@ string rtrim(const string &str)
         find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
     );
     return s;
+}
+
+std::vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
 }
